7f3e68835e10dae4335c578e8e3d7454584b903c
juliapcp <juliapcp2003@gmail.com>
2022-06-11 14:14:42 -0300

    Finalização do projeto

--- index.js
+++ index.js
@@ -19,32 +19,4 @@ app.use('/personagem', PersonagemRouter);
 const CasaRouter = require('./src/harrypotterapi/routes/casa-routes');
 app.use('/casa', CasaRouter);
 
-
-
-// app.get('/', async (req, res) => {
-
-//     // https://swapi.dev/api/films/NUMERO
-
-//     // const numero = Math.floor(1 + Math.random() * 6);
-//     const urlBusca = `https://swapi.dev/api/films/${req.query.filme}`; 
-
-//     try {
-//         const responseApi = await axios.get(urlBusca);
-
-//         res.send({
-//             // numero,
-//             urlBusca,
-//             statusBusca: responseApi.status,
-//             dataBusca: responseApi.data
-//         });
-//     } catch (error) {
-//         console.log({error})
-//         if (error.response.status == 404)
-//             res.send('NAO FOI POSSIVEL LOCALIZAR O RECURSO NA API');
-//         else
-//             res.send('ERRO DESCONHECIDO...');
-//     }
-    
-// });
-
 app.listen(3000, () => console.log("Escutando na porta 3000"));
